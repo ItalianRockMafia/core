7db20e75a663744ef846c16f8f41c2552dd65696
jhuesser <github@jhuesser.ch>
2018-02-16 09:57:25 +0100

    putcalls

--- stationmgmt.php
+++ stationmgmt.php
@@ -2,12 +2,14 @@
 session_start();
 
 $config = require "config.php";
+require 'global/functions/apicalls.php';
 if(isset($_GET['upatestation'])){
 
 	$stationID = $_POST['station'];
-	$userID = $_SESSION['userID'];
-
-
+	$userID = $_SESSION['irmID'];
+	$postfields = "{\n \"stationIDFK\": \"$stationID\" \n}";
+	putCall($config->api_url . "users/" . $userID, $postfields);
+	header('Location: settings.php');
 
 
 }
