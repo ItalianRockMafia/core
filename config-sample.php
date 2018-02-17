37a555c50c759ae07dec7dc4f81f092204b4faad
jhuesser <github@jhuesser.ch>
2018-02-17 13:23:43 +0100

    tg config

--- config-sample.php
+++ config-sample.php
@@ -1,4 +1,9 @@
 <?php
 return (object) array(
-	'api_url' => "ENTER_API_URL_HERE"
+	'api_url' => "ENTER_API_URL_HERE",
+	'telegram' => array(
+		'chatID' => "CHAT_ID_HERE",
+		'bot' => "BOT_USERNAME_HERE",
+		'token' => "BOT_TOKEN_HERE"
+	)
 );
