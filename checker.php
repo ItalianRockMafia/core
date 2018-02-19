779465a9e77f5591498cc35b3490ea490ea6c4f8
jhuesser <github@jhuesser.ch>
2018-02-19 18:48:10 +0100

    navbar fix

--- checker.php
+++ checker.php
@@ -25,11 +25,13 @@ session_start();
       	</li>
 			  <li class="nav-item active">
 				<a class="nav-link" href="#">Settings <span class="sr-only">(current)</span></a>
+
 			  </li>
+			  <li class="nav-item">
+        	<a class="nav-link" href="https://italianrockmafia.ch/meetup">Events</a>
+      			</li>
 				</ul>
-				<li class="nav-item">
-        	<a class="nav-link" href="https://italianrockmafia.ch/meetup">Meetup</a>
-      	</li>
+				
 				<ul class="nav navbar-nav navbar-right">
 				<li class="nav-item">
         			<a class="nav-link" href="login.php?logout=1">Logout</a>
@@ -98,3 +100,12 @@ if($tg_user == false) {
 	$html = 'You need to <a href="login.php">login</a> first.';
 }
 echo $html;
+
+?>
+	</div>
+			</main>
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+				</body>
+			</html>
