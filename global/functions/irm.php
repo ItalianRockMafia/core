8486beecb2dbcd16a3ddff343bd30b511d1cdf1c
jhuesser <jonas.huesser+github@gmail.com>
2018-02-19 10:29:25 +0100

    function added

--- global/functions/irm.php
+++ global/functions/irm.php
@@ -26,3 +26,15 @@ function checkIrmUser($telegramID){
 
 
 }
+
+
+function saveSessionArray($tg_user){
+	$irmarray = json_decode(getCall($config->api_url . "users?transform=1&filter=telegramID,eq," . $tg_user['id']));	
+	$_SESSION['tgID'] = $tg_user['id'];
+	foreach($irmarray['users'] as $irm_user){
+	$_SESSION['irmID'] = $irm_user['userID'];
+	}
+	$_SESSION['username'] = $tg_user['username'];
+	$_SESSION['firstname'] = $tg_user['firstname'];
+	$_SESSION['lastname'] = $tg_user['lastname'];
+}
