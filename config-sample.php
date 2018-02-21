73ee1d6a11a9b7045ef01edf9d007c6cb62d2aa4
jhuesser <github@jhuesser.ch>
2018-02-21 21:30:39 +0100

    added gmaps api key

--- config-sample.php
+++ config-sample.php
@@ -5,5 +5,8 @@ return (object) array(
 		'chatID' => "CHAT_ID_HERE",
 		'bot' => "BOT_USERNAME_HERE",
 		'token' => "BOT_TOKEN_HERE"
+	),
+	'google' => array(
+		'map_api_key' => "ENTER_G_MAPS_API_KEY_HERE"
 	)
 );
