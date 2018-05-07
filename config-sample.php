5e1d23c5458a3ca2a52be749866eb0d8fbc3cde0
jhuesser <github@jhuesser.ch>
2018-05-07 11:53:24 +0200

    last fm config

--- config-sample.php
+++ config-sample.php
@@ -12,5 +12,9 @@ return (object) array(
 	'spotify' => array(
 		'client_id' => "ENTER_SPOTIFY_CLIENT_ID_HERE",
 		'client_secret' => "ENTER_SPOTIFY_CLIENT_SECRET_HERE"
+	),
+	'lastfm' => array(
+		'api_key' => "a6a68923e3eadd7857dc7d97f2ea643d",
+		'api_root' => "http://ws.audioscrobbler.com/"
 	)
 );
