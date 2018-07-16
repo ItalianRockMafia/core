ac8752d3a96ec4e9e9423c4c0048ce8aaebd527d
jhuesser <github@jhuesser.ch>
2018-07-16 11:23:26 +0200

    enalbe custom headers

--- global/functions/header.php
+++ global/functions/header.php
@@ -18,9 +18,13 @@ $header = '
 	<head>
 	<meta charset="utf-8">
  	   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-			<link rel="stylesheet" href="global/main.css">
+			<link rel="stylesheet" href="' . $config->app_url . 'global/main.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<title>'. $options['title'] .'</title>
+	'.
+		$options['custom_header']
+	.'
+
 	</head>
 	<body>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
