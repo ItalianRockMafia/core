b84e33d35fa7dd6033da9fbf8d9980e328bff471
jhuesser <github@jhuesser.ch>
2018-10-11 21:05:24 +0200

    some changes?

---
+++ global/layout/header.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+ 	   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<link rel="stylesheet" href="global/main.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<title>IRM - Settings</title>
+	</head>
+	<body>
+
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+	<a class="navbar-brand" href="#">ItalianRockMafia</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+		<li class="nav-item active">
+        				<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      				</li>
+			  <li class="nav-item">
+				<a class="nav-link" href="settings.php">Settings</a>
+			  </li>
+				<li class="nav-item">
+        	<a class="nav-link" href="<?php echo $config->app_url;?>meetup">Events</a>
+      	</li>
+		  <li class="nav-item">
+        				<a class="nav-link" href="https://italianrockmafia.ch/emp">EMP</a>
+      				</li>
+					  <li class="nav-item">
+        				<a class="nav-link" href="https://italianrockmafia.ch/vinyl">Vinyls</a>
+      				</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				<li class="nav-item">
+        			<a class="nav-link" href="login.php?logout=1">Logout</a>
+      			</li>
+		</ul>
+	</div>
+</nav>
+<div class="topspacer"></div>
